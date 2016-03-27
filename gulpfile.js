9e65a4ca179e5ecafa085cd894b9f6035671ab03
tanbro <RealTanbro@GMail.com>
2016-03-27 22:01:36 +0800

    add babel es6 and uglify, source maps in dist build

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var sourcemaps = require('gulp-sourcemaps');
 var webserver = require('gulp-webserver');
+var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
 
 gulp.task('build-dev', function () {
     return gulp.src('src/*.js')
@@ -24,9 +26,13 @@ gulp.task('serve-dev', function () {
 
 gulp.task('build-dist', function () {
     return gulp.src('src/*.js')
-        .pipe(sourcemaps.init())
         .pipe(jshint()) // run their contents through jshint
         .pipe(jshint.reporter()) // report any findings from jshint
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('dist'));
 });
